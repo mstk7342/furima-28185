5a0a2b7e0c8e0f85cd50d12341a3b8675b1aa6f1
mstk7342 <m7s3t4k228@gmail.com>
2020-11-14 16:08:01 +0900

    ユーザー管理機能【単体テストコード訂正】

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,7 +29,18 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-
+      it "emailに@がない時に登録できない" do
+        @user.email = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include()
+      end
+      it "重複したemailが存在する場合登録できない" do
+        @user.save
+        another_user = FactoryBot.build(:user)
+        another_user.email = @user.email
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include("Email has already been taken")
+      end
       it "passwordは空では登録できない" do
         @user.password = ""
         @user.valid?
